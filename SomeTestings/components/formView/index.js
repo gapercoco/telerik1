183da7acc80546d524a9b244d83712fd6f3deaca
Alejandro Garavelli <agaravelli@overall.com.ar>
2016-02-23 15:47:56 +0000

    Changes from Views service

--- SomeTestings/components/formView/index.js
+++ SomeTestings/components/formView/index.js
@@ -12,6 +12,7 @@ app.formView = kendo.observable({
 (function(parent) {
     var formViewModel = kendo.observable({
         fields: {
+            group: '',
             formResponse: '',
             password: '',
             username: '',
@@ -26,4 +27,34 @@ app.formView = kendo.observable({
 // START_CUSTOM_CODE_formViewModel
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
+app.formView.formViewModel.myLogin = function() {
+
+    var that = this;
+    $.ajax({
+        method: "POST",
+        url: 'http://overall.donweb-remoteip.net:27025/auth/local',
+        //contentType: 'application/json',
+        dataType: 'json',
+        data: {
+            email: that.formViewModel.fields.username,
+            password: that.formViewModel.fields.password,
+            deviceUuid: 'e0908060g38bde8e6740011221af335301010333'
+
+        }
+    }).then(function(data) {
+        console.info('Ajax OK.');
+        console.table(data);
+        if (typeof data.token != 'undefined' && data.token != null) {
+            console.info('Auth OK');
+            that.formViewModel.fields.formResponse = 'Login valido'
+        } else {
+            console.warn('Auth NOT OK.');
+            that.formViewModel.fields.formResponse = data.message;
+        }
+    }, function(error) {
+        console.warn('Houston we have a problem... auth NOT ok');
+        console.table(error);
+        that.formViewModel.fields.formResponse = error.responseText.message;
+    });
+};
 // END_CUSTOM_CODE_formViewModel
